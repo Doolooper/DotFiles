9929a31aa9ed8007fb3f344ad2cbd0143500e196
Mahdi Harati <doolooper@outlook.com>
2023-06-29 13:55:25 -0400

    add zsh

---
+++ zsh/alias.zsh
@@ -0,0 +1,5 @@
+alias cls=clear
+alias ls='exa -l'
+alias gg=lazygit
+alias bu='brew update && brew upgrade && brew cleanup'
+
---
+++ zsh/zsh.zsh
@@ -0,0 +1,24 @@
+eval "$(starship init zsh)"
+source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.zsh/completion.zsh
+source $HOME/.zsh/history.zsh
+source $HOME/.zsh/alias.zsh
+
+# Initialize the completion system
+autoload -Uz compinit
+
+# Cache completion if nothing changed - faster startup time
+typeset -i updated_at=$(date +'%j' -r ~/.zcompdump 2>/dev/null || stat -f '%Sm' -t '%j' ~/.zcompdump 2>/dev/null)
+if [ $(date +'%j') != $updated_at ]; then
+  compinit -i
+else
+  compinit -C -i
+fi
+
+# Enhanced form of menu completion called `menu selection'
+zmodload -i zsh/complist
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
