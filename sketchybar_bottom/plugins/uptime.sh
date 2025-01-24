121abbb5a6d63cafe75abed8381d721a1a8fb02a
Mahdi Harati <doolooper@outlook.com>
2025-01-24 09:16:47 -0500

    backup updated on 2025-01-24 09:16

--- sketchybar_bottom/plugins/uptime.sh
+++ sketchybar_bottom/plugins/uptime.sh
@@ -1,3 +1,3 @@
-uptime=$(uptime | awk -F'(up |, 1 user)' '{print $2}' | awk -F'( days,  |:)' '{print $1"d "$2"h "$3"m"}')
+uptime=$(uptime | awk -F'(up |, 1 user)' '{print $2}' | awk -F'( days, |:)' '{print $1"d "$2"h "$3"m"}' | awk '{print $1" "$2" "$3}')
 
 sketchybar_bottom --set uptime label="$uptime"
