676fea0aae5013d9a627ca6c46200705e88ac878
Mahdi Harati <doolooper@outlook.com>
2025-02-03 19:27:32 -0500

    backup updated on 2025-02-03 19:27

--- sketchybar_bottom/items/package_manager.sh
+++ sketchybar_bottom/items/package_manager.sh
@@ -5,4 +5,5 @@ sketchybar_bottom --add item homebrew right \
     update_freq=900 \
     background.color=$PAKCAGE_MANAGER_COLOR \
     background.border_color=$PAKCAGE_MANAGER_COLOR \
-    script="$PLUGIN_DIR/package_manager.sh"
+    script="$PLUGIN_DIR/package_manager.sh" \
+    --subscribe homebrew system_woke
