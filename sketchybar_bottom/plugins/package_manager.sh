d8311537682fd7e9611b66476f1e89c08139e36c
Mahdi Harati <doolooper@outlook.com>
2025-04-18 14:18:57 -0400

    backup updated on 2025-04-18 14:18

--- sketchybar_bottom/plugins/package_manager.sh
+++ sketchybar_bottom/plugins/package_manager.sh
@@ -3,20 +3,7 @@ update() {
     sketchybar_bottom --set homebrew label=$BREW_OUTDATED
 }
 
-tmux_update_first_session() {
-    local session=$(tmux list-sessions -F '#S' 2> /dev/null | head -n 1)
-
-    if [[ -z "$session" ]]; then
-        return 1
-    fi
-
-    tmux send-keys -t "${session}:1" 'update' C-m
-}
-
 case "$SENDER" in
-    "mouse.clicked")
-        tmux_update_first_session
-        ;;
     *)
         update
         ;;
