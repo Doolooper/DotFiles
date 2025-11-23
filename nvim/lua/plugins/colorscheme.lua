6ed3c516f10f1915d3ad200c0f4844a6c95d0ec1
Mahdi Harati <doolooper@outlook.com>
2025-11-23 18:54:33 -0500

    backup updated on 2025-11-23 18:54

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,49 +5,18 @@ return {
     priority = 1000,
   },
   {
-    "mcauley-penney/techbase.nvim",
+    "webhooked/kanso.nvim",
     lazy = false,
     priority = 1000,
-  },
-  {
-    "fcancelinha/nordern.nvim",
-    lazy = false,
-    priority = 1000,
-    branch = "master",
-  },
-  {
-    "killitar/obscure.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "gmr458/cold.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "aktersnurra/no-clown-fiesta.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "lucasadelino/conifer.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {},
-  },
-  {
-    "chama-chomo/grail",
-    version = false,
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "wheat-thin-wiens/rei.nvim",
-    priority = 1000,
-    opts = {},
+    opts = {
+      italics = false,
+      background = {
+        dark = "ink",
+        light = "ink",
+      },
+    },
     config = function()
-      vim.cmd("colorscheme rei")
+      vim.cmd("colorscheme kanso-ink")
     end,
   },
 }
